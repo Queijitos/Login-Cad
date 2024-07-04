355fe573b87f845b347bbff9da9acd400e8666cc
JOSE WALTER DE OLIVEIRA JUNIOR <walter.jose@alunoescolassociais.org.br>
2024-07-04 11:47:59 -0300

    colocando mais coisas no javaScript

--- control.js
+++ control.js
@@ -1,19 +1,19 @@
 //Função para validação de acesso
-function acessar(){
-let loginEmail = document.getElementById('loginEmail').value;
-let loginSenha = document.getElementById('loginSenha').value;
+function acessar() {
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
 
-if(!loginEmail || !loginSenha){
-    alert("Favor preencher todos os campos");
-}
-else{
-    // alert("Campos preenchidos com sucesso");
-    window.location.href = 'cadastro.html'
-};
+    if (!loginEmail || !loginSenha) {
+        alert("Favor preencher todos os campos");
+    }
+    else {
+        // alert("Campos preenchidos com sucesso");
+        window.location.href = 'cadastro.html'
+    };
 }
 
 //Funçaõ para armazenamento de nomes em array
 var dadosLista = [];
-function salvarUser(){
-    
+function salvarUser() {
+
 }
