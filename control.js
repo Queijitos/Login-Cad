c3094fa36a9b3bf87f4c80b881cb8b27da15cf0e
JOSE WALTER DE OLIVEIRA JUNIOR <walter.jose@alunoescolassociais.org.br>
2024-08-16 08:10:22 -0300

    adicionando melhorias para o JavaScript

--- control.js
+++ control.js
@@ -15,5 +15,34 @@ function acessar() {
 //Funçaõ para armazenamento de nomes em array
 var dadosLista = [];
 function salvarUser() {
+let nomeUser = document.getElementById('nomeUser').value;
 
+if(nomeUser){
+    dadosLista.push(nomeUser);
+    // console.log(dadosLista);
+    CriarLista();
+    document.getElementById('nomeUser').value = "";
+
+}else{
+    alert("Favor informe um nome para cadastro");
+}
+
+}
+// função para criar lista de usuario
+function CriarLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>nome Usuario </th><th>Ações<th><tr>";
+    for(let i=0;i<=(dadosLista.length - 1);i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='Editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='Excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+// função para editar nomes da lista
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista [(i-1)];
+    dadosLista.splice(dadosLista[(i-1)],1);
+}
+// Função para excluir nome de lista
+function excluir (i){
+    dadosLista.splice((i - 1),1);
+    document.getElementById('tabela').deleterow(i);
 }
